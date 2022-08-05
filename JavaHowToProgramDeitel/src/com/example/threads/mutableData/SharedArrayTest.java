f41d429037edd277f89ee2e9ce25b686a5be76b9
Aritana Noara Costa Santos <aritanacs@ciandt.com>
2022-08-05 17:22:58 -0300

    Dados mutáveis não sincronizados

---
+++ JavaHowToProgramDeitel/src/com/example/threads/mutableData/SharedArrayTest.java
@@ -0,0 +1,44 @@
+package com.example.threads.mutableData;
+
+import java.lang.reflect.Array;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class SharedArrayTest {
+
+  public static void main(String[] arg){
+
+    //constrói o bojeto compartilhado.
+    SimpleArray sharedSimpleArray = new SimpleArray(6);
+
+    //cria duas tarefas a gravar no SimpleArray compartilhado
+
+    ArrayWriter writer1 = new ArrayWriter(1, sharedSimpleArray);
+    ArrayWriter writer2 = new ArrayWriter(11, sharedSimpleArray);
+
+    //executa as tarefas com um ExecutorService
+
+    ExecutorService executorService = Executors.newCachedThreadPool();
+    executorService.execute(writer1);
+    executorService.execute(writer2);
+
+    executorService.shutdown();
+
+    try {
+
+      //espera 1 minuto para que ambos os escritores terminem a execucao
+      boolean taskEnded =  executorService.awaitTermination(1, TimeUnit.MINUTES);
+
+      if(taskEnded){
+        System.out.printf("%nContents of SimpleArray:%n");
+        System.out.println(sharedSimpleArray); //imprime o conteudo
+      }else {
+        System.out.println("Timed out while waiting for tasks to finish.");
+      }
+    }catch (InterruptedException ex){
+      ex.printStackTrace();
+    }
+  }
+
+}
---
+++ JavaHowToProgramDeitel/src/com/example/threads/mutableData/SimpleArray.java
@@ -0,0 +1,42 @@
+package com.example.threads.mutableData;
+
+import java.security.SecureRandom;
+import java.util.Arrays;
+
+public class SimpleArray {  // ATENÇÃO: NÃO SEGURO PARA THREADS!
+
+
+  private static final SecureRandom generator  =   new SecureRandom();
+  private final int[] array; //array de inteiros compartilhado
+  private int writeIndex = 0; //indice compartilhado do próximo elemento a gravar
+
+  //cria uma SimpleArray de um determinado tamanho
+  public SimpleArray(int size) {
+    array = new int[size];
+  }
+
+  public void add(int value){
+
+    int position = writeIndex;
+
+    try {
+      //coloca a thread para dormir durante 0 a 499 milissegundos.
+      Thread.sleep(generator.nextInt(500));
+
+    }catch (InterruptedException ex){
+
+      Thread.currentThread().interrupt();
+    }
+    //coloca o o valor no elemento apropriado
+    array[position] =  value;
+    System.out.printf("%s wrote %2d to element %d.%n", Thread.currentThread().getName(), value, position);
+
+    ++writeIndex;
+    System.out.printf("Next write index: %d%n", writeIndex);
+  }
+
+  public String toString(){
+    return Arrays.toString(array);
+  }
+
+}
