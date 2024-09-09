bc709e2d9056d0ef33dce254272192b2e67889a6
Aritana Noara Santos <arinoara@yahoo.com.br>
2024-09-09 17:35:02 -0300

    feature: removendo textos de explicação do teste

--- JavaHowToProgramDeitel/src/com/example/groupSoftwareTest/Tarefa1.java
+++ JavaHowToProgramDeitel/src/com/example/groupSoftwareTest/Tarefa1.java
@@ -1,16 +1,7 @@
 package com.example.groupSoftwareTest;
 
-import java.io.FileFilter;
 import java.util.ArrayList;
 import java.util.List;
-
-/**
- * Considerando os tipos de figuras geométricas como quadrado, triângulo, retângulo e círculo.
- * Desenvolva uma classe em Java que armazene calcule a área total das figuras,
- * essa classe deve ser capaz de imprimir a somatória das áreas no console.
- * Cada figura é representada por uma classe e suas propriedades devem ser armazenadas de acordo com o tipo da figura.
- * *Utilize os conceitos de orientação a objetos como herança, polimorfismo e encapsulamento para resolver o problema.
- */
 
 abstract class Figura {
     abstract Double getArea();
