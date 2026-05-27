df5ff1b0931d9f2ecf5a8aa063bcbc7a012ca795
noara <arinoara@yahoo.com.br>
2026-05-27 14:42:45 -0300

    feat: ApplicationDefaultt

---
+++ code-with-quarkus/src/main/java/com/aritana/ApplicationDefaultt.java
@@ -0,0 +1,8 @@
+package com.aritana;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("api")
+public class ApplicationDefaultt extends Application {
+}
--- code-with-quarkus/src/main/java/com/aritana/UniPDSResource.java
+++ code-with-quarkus/src/main/java/com/aritana/UniPDSResource.java
@@ -1,9 +1,6 @@
 package com.aritana;
 
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.media.Content;
@@ -11,23 +8,48 @@ import org.eclipse.microprofile.openapi.annotations.media.Schema;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
+import java.time.LocalDateTime;
+
 @Tag(name = "UniPDS", description = "Endpoints UniPDS")
 @Path("/unipds")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class UniPDSResource {
 
-    private int i = 0;
+    private final int i = 0;
 
-    @GET
+
     @Operation(summary = "Retorna o valor atual de i")
     @APIResponse(
             responseCode = "200",
             description = "Valor inteiro de i",
             content = @Content(schema = @Schema(implementation = Integer.class))
     )
-    @Produces(MediaType.APPLICATION_JSON)
+
+    @GET
     public int getI(){
         return i;
     }
+
+    @GET
+    @Path("getLocalDateTime")
+    public LocalDateTime data(){
+        return LocalDateTime.now();
+    }
+
+    @POST
+    public void addI(){
+
+    }
+
+    @DELETE
+    public void remove(){
+
+    }
+
+    @PUT
+    public void setI(){
+
+    }
+
 }
