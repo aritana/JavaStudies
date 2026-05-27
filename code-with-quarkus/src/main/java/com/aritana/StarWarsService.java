0056cc385aeeca68bc41880a6edbe9b7f4a3176b
noara <arinoara@yahoo.com.br>
2026-05-27 14:57:14 -0300

    feat: rest client

---
+++ code-with-quarkus/src/main/java/com/aritana/StarWarsService.java
@@ -0,0 +1,16 @@
+package com.aritana;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+@RegisterRestClient(baseUri = "https://swapi.info/api/")
+public interface StarWarsService {
+
+    @GET
+    @Path("starships")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getStarShips();
+}
